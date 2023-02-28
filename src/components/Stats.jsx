f58daccb01a3f90fec04963bb3a37e99a2aee5ed
yusufseward <yusufseward@gmail.com>
2023-02-28 13:33:16 -0600

    finished hero

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { stats } from "../constants";
+import styles from "../style";
 
 const Stats = () => {
   return (
@@ -6,4 +7,4 @@ const Stats = () => {
   )
 }
 
-export default Stats
+export default Stats;
